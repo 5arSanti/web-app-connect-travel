ad0373917a60dcca21232013b074ae5bb8c6633e
Santiago Arias <santiari05@hotmail.com>
2025-07-04 00:26:23 -0500

    Refactor Footer component to streamline structure and improve readability. Consolidate JSX elements and ensure consistent formatting for better maintainability.

--- src/Pages/components/Footer/index.jsx
+++ src/Pages/components/Footer/index.jsx
@@ -10,49 +10,30 @@ import { MdOpenInNew } from "react-icons/md";
 import { NavButtons } from "../NavButtons";
 
 const Footer = () => {
+    const date = new Date();
+
     return (
-        <>
-            <footer className="footer">
-                <GridContainer className="grid-075-125">
+        <footer className="footer">
+            <WrapperContainer2 flexDirection="column" gap={75}>
+                <WrapperContainer2 justifyContent="center" alignItems="center" gap={5} flexDirection="column">
+                    <Link to={"/home"}>
+                        <Title className="animacion2" white={true}>CONNECT TRAVEL'S</Title>
+                        <TextCard width="auto" className="animacion2" white={true} textAlign="center">TU CONEXIÓN CON EL MUNDO</TextCard>
+                    </Link>
+                </WrapperContainer2>
 
-                    <WrapperContainer2 flexDirection="column" gap={75}>
-
-
-                        <WrapperContainer2 justifyContent="center" alignItems="center" gap={5} flexDirection="column">
-                            <Link to={"/home"}>
-                                <Title className="animacion2" white={true}>CONNECT TRAVEL'S</Title>
-                                <TextCard width="auto" className="animacion2" white={true} textAlign="center">TU CONEXIÓN CON EL MUNDO</TextCard>
-                            </Link>
-                        </WrapperContainer2>
-
-                        <GridContainer className="grid-1-1-1">
-                            <NavButtons/>
-                        </GridContainer>
-
-                        <WrapperContainer2 justifyContent="center" alignItems="center" padding={"40px 0px 0px 0px"} className="border-top">
-                            <TextCard width="auto" className="privacy-policy animacion2" fontSize={13} white={true} textAlign="center">Politica de privacidad <MdOpenInNew/></TextCard>
-                        </WrapperContainer2>
-                    </WrapperContainer2>
+                <GridContainer className="grid-1-1-1">
+                    <NavButtons />
                 </GridContainer>
 
-            </footer>
-            <SecondFooter/>
-        </>
+                <WrapperContainer2 justifyContent="center" alignItems="center" padding={"40px 0px 0px 0px"} className="border-top">
+                    <TextCard width="auto" className="privacy-policy animacion2" fontSize={13} white={true} textAlign="center">Politica de privacidad <MdOpenInNew /></TextCard>
+                </WrapperContainer2>
+            </WrapperContainer2>
+
+            <TextCard white={true} fontSize={10}>Copyright &copy; {date.getFullYear()} Connect Travel's</TextCard>
+        </footer>
     );
 }
 
-const SecondFooter = () => {
-    const date = new Date();
-    
-    return (
-        <WrapperContainer2 
-            className="footer-copy-container"
-            padding={"20px 150px"}
-            justifyContent="space-between"
-        >
-            <TextCard fontSize={10}>Copyright &copy; {date.getFullYear()} Connect Travel's</TextCard>
-        </WrapperContainer2>
-    );
-}
-
-export {Footer};
+export { Footer };
