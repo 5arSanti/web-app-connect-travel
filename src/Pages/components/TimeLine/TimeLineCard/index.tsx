38599b09300add05396713b8fc267424cc1f61a3
Santiago Arias <santiari05@hotmail.com>
2025-07-07 23:22:45 -0500

    Refactor TimelineCard component to enhance layout and styling. Integrate WrapperContainers and FadeWrapper for improved structure and animations. Update styles for better visual consistency and adjust timeline-dot color for better contrast.

--- src/Pages/components/TimeLine/TimeLineCard/index.tsx
+++ src/Pages/components/TimeLine/TimeLineCard/index.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 import { TextCard } from "../../TextComponents";
 import "./styles.css"
+import { WrapperContainer1, WrapperContainer2 } from "../../WrapperContainers";
+import { FadeWrapper } from "../../FadeWrapper";
 
 interface TimelineCardProps {
     icon: React.ReactNode
@@ -13,14 +15,30 @@ const TimelineCard = ({ icon, title, description, side }: TimelineCardProps) => 
     let align = side === "left" ? "end" : "start"
     return (
         <>
-            <div className={`timeline-card ${side}`}>
+            <WrapperContainer2
+                className={`timeline-card ${side}`}
+                padding={"0px 20px" as unknown as number}
+            >
                 <div className={`timeline-dot ${side}`} />
-                <div className="timeline-card-content" style={{ alignItems: align }}>
-                    <div className="timeline-icon">{icon}</div>
-                    <h3 className="timeline-title">{title}</h3>
-                    <TextCard textAlign={align} className="timeline-description">{description}</TextCard>
-                </div>
-            </div>
+                <WrapperContainer1
+                    className="timeline-card-content shadow-style"
+                    flexDirection="column"
+                    borderRadius={10}
+                    gap={10}
+                    alignItems={align}
+                    padding="20px 30px"
+                >
+                    <FadeWrapper delay={0}>
+                        <WrapperContainer2 padding={0} justifyContent={align} alignItems="center" gap={10}>
+                            <div className="timeline-icon">{icon}</div>
+                            <TextCard fontSize={20} width="auto" className="timeline-title">{title}</TextCard>
+                        </WrapperContainer2>
+                    </FadeWrapper>
+                    <FadeWrapper delay={100}>
+                        <TextCard textAlign={align} className="timeline-description">{description}</TextCard>
+                    </FadeWrapper>
+                </WrapperContainer1>
+            </WrapperContainer2>
             <div />
         </>
     )
